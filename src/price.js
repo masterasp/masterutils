ceeccf2e38f99a16cf2d2897a9bb938bdea9c76e
Jordi Baylina <jordi@baylina.cat>
2015-09-07 11:29:09 +0200

    Price refactored

--- src/price.js
+++ src/price.js
@@ -12,106 +12,107 @@ var Price = function(lines) {
     if (lines.lines) {
         lines = lines.lines;
     }
-    var self = this;
 
 // Clone the array;
-    self.lines = _.map(lines, _.clone);
+    this.lines = _.map(lines, _.clone);
+};
 
-    self.toJSON = function() {
-        var obj = {};
-        obj.lines = _.map(self.lines, _.clone);
-        _.each(obj.lines, function(l) {
-            if (typeof l.from === "number") l.from = du.int2date(l.from);
-            if (typeof l.to === "number") l.to = du.int2date(l.to);
-        });
-        return obj;
-    };
+Price.prototype.toJSON = function() {
+    var obj = {};
+    obj.lines = _.map(this.lines, _.clone);
+    _.each(obj.lines, function(l) {
+        if (typeof l.from === "number") l.from = du.int2date(l.from);
+        if (typeof l.to === "number") l.to = du.int2date(l.to);
+    });
+    return obj;
+};
 
-    self.lineImport = function(line, options) {
-        options = options || {};
-        options = _.extend({
-            withTaxes: true,
-            withDiscounts: true,
-            rounded: true,
-            base: 0
-        }, options);
+Price.prototype.lineImport = function(line, options) {
+    options = options || {};
+    options = _.extend({
+        withTaxes: true,
+        withDiscounts: true,
+        rounded: true,
+        base: 0
+    }, options);
 
-        var price;
-        if (typeof line.price === "number") {
-            price = line.price;
-        } else if ( (line.price==="object") && (line.price.type === 'ESC') ) {
-            price = options.base * line.price.pricePC;
-            if (price<line.price.priceMin) price = line.price.priceMin;
-        } else if ( (line.price==="object") && (line.price.type === 'ESC') ) {
-            price=Number.MAX_VALUE;
-            _.each(line.price.scalePrices, function(sp) {
-                if ((base <= sp.stayPriceMax) && (sp.price < price)) {
-                    price = sp.price;
-                }
-            });
-            if (price === Number.MAX_VALUE) {
-                price = NaN;
+    var price;
+    if (typeof line.price === "number") {
+        price = line.price;
+    } else if ( (line.price==="object") && (line.price.type === 'ESC') ) {
+        price = options.base * line.price.pricePC;
+        if (price<line.price.priceMin) price = line.price.priceMin;
+    } else if ( (line.price==="object") && (line.price.type === 'ESC') ) {
+        price=Number.MAX_VALUE;
+        _.each(line.price.scalePrices, function(sp) {
+            if ((base <= sp.stayPriceMax) && (sp.price < price)) {
+                price = sp.price;
             }
+        });
+        if (price === Number.MAX_VALUE) {
+            price = NaN;
         }
+    }
 
-        var lineImport = price * line.quantity;
-        if (!isNaN(line.periods)) {
-            lineImport = lineImport * line.periods;
-        }
+    var lineImport = price * line.quantity;
+    if (!isNaN(line.periods)) {
+        lineImport = lineImport * line.periods;
+    }
 
-        if (options.withDiscounts) {
-            var base = lineImport;
-            _.each(line.discounts, function(discount) {
-                if (discount.type === "PC") {
-                    lineImport = lineImport - base * discount.PC/100;
-                }
-            });
-        }
+    if (options.withDiscounts) {
+        var base = lineImport;
+        _.each(line.discounts, function(discount) {
+            if (discount.type === "PC") {
+                lineImport = lineImport - base * discount.PC/100;
+            }
+        });
+    }
 
-        if (options.withTaxes) {
-            _.each(line.taxes, function(tax) {
-                if (tax.type=== "VAT") {
-                    lineImport = lineImport * (1 + tax.PC/100);
-                }
-            });
-        }
+    if (options.withTaxes) {
+        _.each(line.taxes, function(tax) {
+            if (tax.type=== "VAT") {
+                lineImport = lineImport * (1 + tax.PC/100);
+            }
+        });
+    }
 
-        if (options.rounded) {
-            lineImport = round(lineImport, "ROUND", 0.01);
-        }
+    if (options.rounded) {
+        lineImport = round(lineImport, "ROUND", 0.01);
+    }
 
-        return lineImport;
-    };
+    return lineImport;
+};
 
-    self.getImport = function(options) {
-        options = options || {};
-        options = _.extend({
-            withTaxes: true,
-            withDiscounts: true,
-            rounded: true
-        }, options);
+Price.prototype.getImport = function(options) {
+    var self = this;
+    options = options || {};
+    options = _.extend({
+        withTaxes: true,
+        withDiscounts: true,
+        rounded: true
+    }, options);
 
-        var oldRounded = options.rounded;
+    var oldRounded = options.rounded;
 
-        options.rounded = false;
-        var ac = _.reduce(self.lines, function(memo, line) {
-            return memo + self.lineImport(line, options);
-        },0);
+    options.rounded = false;
+    var ac = _.reduce(self.lines, function(memo, line) {
+        return memo + self.lineImport(line, options);
+    },0);
 
-        if (oldRounded) {
-            ac = round(ac, "ROUND", 0.01);
-        }
+    if (oldRounded) {
+        ac = round(ac, "ROUND", 0.01);
+    }
 
-        return ac;
-    };
+    return ac;
+};
 
-    self.addPrice = function(p) {
+Price.addPrice = function(p) {
+        var self = this;
         if (!p) return;
         _.each(p.lines, function(l) {
             self.lines.push(_.clone(l));
         });
-    };
-
 };
+
 
 module.exports = Price;
